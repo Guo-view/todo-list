13bdd715d3dcc0a63ac90536d2c1bfb7789cb576
your name <your email>
2022-07-24 18:31:40 +0800

    周末作业

---
+++ 周末作业/types.ts
@@ -0,0 +1,142 @@
+// let obj:{name:string,age:number}
+// let obj1:{name:string,age:number}
+// // function createPerson(name:string,age:number){
+// //     let o=new Object();
+// //     o.name=name,
+// //     o.age=age
+// // }
+// // 第一题
+// let booleanarrays:boolean[]=[true,false]
+// let booleanarray:Array<boolean>=[true,false,false]
+// // 第三题
+// obj={name:"郭俊",age:13}
+// obj1={name:'君子',age:12}
+// // document.body.innerHTML=`my name is ${obj.name},my age is ${obj.age}`
+// let grenxinxi:[name:string,age:number,height:number,price:boolean]=['郭俊',23,178,false]
+// // 第四题
+// // for(var key in grenxinxi){
+// //     document.body.innerHTML=`my name is ${grenxinxi[key]} age is ${grenxinxi[key]} 身高 is ${grenxinxi[key]} 是否工作 ${grenxinxi[key]}`
+// // }
+// // 经典不能用
+// enum Age{
+//     Guojun=23,
+//     Cuiming=23,
+//     Ccc=18
+// }
+// let c:Age
+// let value:any;
+// value=12
+// value='wode'
+// value=false
+
+// function alias():void{
+//     console.log('你谁啊');
+    
+// }
+
+// // never
+// // 返回never的函数必须存在无法达到的终点
+// function error(message: string): never {
+//     throw new Error(message);
+// }
+
+// // 推断的返回值类型为never
+// function fail() {
+//     return error("Something failed");
+// }
+
+// let obj:{name:string,age:number}
+// let obj1:{name:string,age:number}
+// // function createPerson(name:string,age:number){
+// //     let o=new Object();
+// //     o.name=name,
+// //     o.age=age
+// // }
+// // 第一题
+// let booleanarrays:boolean[]=[true,false]
+// let booleanarray:Array<boolean>=[true,false,false]
+// // 第三题
+// obj={name:"郭俊",age:13}
+// obj1={name:'君子',age:12}
+// // document.body.innerHTML=`my name is ${obj.name},my age is ${obj.age}`
+// let grenxinxi:[name:string,age:number,height:number,price:boolean]=['郭俊',23,178,false]
+// // 第四题
+// // for(var key in grenxinxi){
+// //     document.body.innerHTML=`my name is ${grenxinxi[key]} age is ${grenxinxi[key]} 身高 is ${grenxinxi[key]} 是否工作 ${grenxinxi[key]}`
+// // }
+// // 经典不能用
+// enum Age{
+//     Guojun=23,
+//     Cuiming=23,
+//     Ccc=18
+// }
+// let c:Age
+// let value:any;
+// value=12
+// value='wode'
+// value=false
+
+// function alias():void{
+//     console.log('你谁啊');
+    
+// }
+
+// // never
+// // 返回never的函数必须存在无法达到的终点
+// function error(message: string): never {
+//     throw new Error(message);
+// }
+
+// // 推断的返回值类型为never
+// function fail() {
+//     return error("Something failed");
+// }
+
+
+let obj:{name:string,age:number}
+let obj1:{name:string,age:number}
+// function createPerson(name:string,age:number){
+//     let o=new Object();
+//     o.name=name,
+//     o.age=age
+// }
+// 第一题
+let booleanarrays:boolean[]=[true,false]
+let booleanarray:Array<boolean>=[true,false,false]
+// 第三题
+obj={name:"郭俊",age:13}
+obj1={name:'君子',age:12}
+// document.body.innerHTML=`my name is ${obj.name},my age is ${obj.age}`
+let grenxinxi:[name:string,age:number,height:number,price:boolean]=['郭俊',23,178,false]
+// 第四题
+// for(var key in grenxinxi){
+//     document.body.innerHTML=`my name is ${grenxinxi[key]} age is ${grenxinxi[key]} 身高 is ${grenxinxi[key]} 是否工作 ${grenxinxi[key]}`
+// }
+// 经典不能用
+enum Age{
+    Guojun=23,
+    Cuiming=23,
+    Ccc=18
+}
+let c:Age
+let value:any;
+value=12
+value='wode'
+value=false
+
+function alias():void{
+    console.log('你谁啊');
+    
+}
+
+// never
+// 返回never的函数必须存在无法达到的终点
+function error(message: string): never {
+    throw new Error(message);
+}
+
+// 推断的返回值类型为never
+function fail() {
+    return error("Something failed");
+}
+
