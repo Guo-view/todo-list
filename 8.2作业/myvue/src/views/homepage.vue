c903e265eff7e7341cfd473eda893b4503f7459d
your name <your email>
2022-08-02 07:45:02 +0800

    8.2

---
+++ 8.2作业/myvue/src/views/homepage.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>梦学谷学生管理系统欢迎你</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyvueHomepage',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
